2f24798b216288b1b0d5fc5468154eee2b663b2e
Asim14878111 <asim14875111@gmail.com>
2025-04-17 21:53:12 +0500

    Added one delete function for all delete buttons

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
 function Deleteitem() {
-
-
-
   const deleteButton = document.querySelectorAll('.deleteitem');
 
 
@@ -12,133 +9,22 @@ function Deleteitem() {
         const taskRow = button.closest('.tasktodo');
            taskRow.remove();
         
+// console.log('Clicked')
+
      });
   });
 }
 
+function toggleEnable(id){
+   var textbox = document.getElementById(id);
+   if(textbox.disabled){
+      document.getElementById(id).disabled = false;
+   }
+   else{
 
-const inputfield = document.getElementById('first-input');
-const editButton = document.getElementById('editButton')
-editButton.addEventListener('click', function () {
-  inputfield.disabled = !inputfield.disabled;
-  if (inputfield.disabled) {
-     editButton.textContent = 'Edit'
-  } else {
-     editButton.textContent = "Save"
-  }
-})
+      document.getElementById(id).disabled = true;}
+}
 
-// 2nd row starts
-const secondinputfield = document.getElementById('2nd-input-field');
-const editbutton = document.getElementById('editbutton');
-
-editbutton.addEventListener('click', function () {
-  secondinputfield.disabled = !secondinputfield.disabled;
-  if (secondinputfield.disabled) {
-     editbutton.textContent = 'Edit'
-  } else {
-     editbutton.textContent = "Save"
-  }
-})
-
-// 3rd row starts
-
-const editinput = document.getElementById('editinput');
-const edittask = document.getElementById('edittask');
-
-
-edittask.addEventListener('click', function () {
-  editinput.disabled = !editinput.disabled;
-  if (editinput.disabled) {
-     edittask.textContent = 'Edit'
-  } else {
-     edittask.textContent = "Save"
-  }
-})
-
-
-//  4th row
-
-const editablefirstprogess = document.getElementById('editablefirstprogess');
-const editprogress1 = document.getElementById('editprogress1');
-
-
-editprogress1.addEventListener('click', function () {
-  editablefirstprogess.disabled = !editablefirstprogess.disabled;
-  if (editablefirstprogess.disabled) {
-     editprogress1.textContent = 'Edit'
-  } else {
-     editprogress1.textContent = "Save"
-  }
-})
-
-// 5th row
-
-const editablesecondprogess = document.getElementById('editablesecondprogess');
-const editprogress2 = document.getElementById('editprogress2');
-
-editprogress2.addEventListener('click', function () {
-  editablesecondprogess.disabled = !editablesecondprogess.disabled;
-  if (editablesecondprogess.disabled) {
-     editprogress2.textContent = 'Edit'
-  } else {
-     editprogress2.textContent = "Save"
-  }
-})
-
-//  6th row
-
-const editablethirdprogess = document.getElementById('editablethirdprogess');
-const editprogress3 = document.getElementById('editprogress3');
-
-editprogress3.addEventListener('click', function () {
-  editablethirdprogess.disabled = !editablethirdprogess.disabled;
-  if (editablethirdprogess.disabled) {
-     editprogress3.textContent = 'Edit'
-  } else {
-     editprogress3.textContent = "Save"
-  }
-})
-
-//  7th row
-
-const editableone = document.getElementById('editableone');
-const editcompleted1 = document.getElementById('editcompleted1');
-
-editcompleted1.addEventListener('click', function () {
-  editableone.disabled = !editableone.disabled;
-  if (editableone.disabled) {
-     editcompleted1.textContent = 'Edit'
-  } else {
-     editcompleted1.textContent = "Save"
-  }
-})
-
-//  8th row
-
-
-const editabletwo = document.getElementById('editabletwo');
-const editcompleted2 = document.getElementById('editcompleted2');
-
-editcompleted2.addEventListener('click', function () {
-  editabletwo.disabled = !editabletwo.disabled;
-  if (editabletwo.disabled) {
-     editcompleted2.textContent = 'Edit'
-  } else {
-     editcompleted2.textContent = "Save"
-  }
-})
-
-//  9th row 
-const editablethree = document.getElementById('editablethree');
-const editcompleted3 = document.getElementById('editcompleted3');
-editcompleted3.addEventListener('click', function () {
-  editablethree.disabled = !editablethree.disabled;
-  if (editablethree.disabled) {
-     editcompleted3.textContent = 'Edit'
-  } else {
-     editcompleted3.textContent = "Save"
-  }})
 
 
 const openBtn = document.getElementById('openModal')
@@ -165,8 +51,11 @@ function cancelentries(){
   loginform.reset()
 }
 
+// Creating array to stores new entries 
+
 let data = []
 
+// Adding new entries
 
 function addEntry() {
   const newentry = document.getElementById('add-entry').value
